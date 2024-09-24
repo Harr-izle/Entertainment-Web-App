6eee3875d0c86106be129c433ab323059f468c97
Harr-izle <harrietbuadee@gmail.com>
2024-09-24 20:28:09 +0000

    designs login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./components/nav/nav.component";
 import { SearchComponent } from "./components/search/search.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, SearchComponent],
+  imports: [RouterOutlet, NavComponent, SearchComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
