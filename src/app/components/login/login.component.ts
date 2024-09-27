9756062e64cc782760fcf9d56149e4f2b77e512b
Harr-izle <harrietbuadee@gmail.com>
2024-09-27 15:43:06 +0000

    implements logi for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,24 +1,36 @@
-
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  errorMessage: string | undefined;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
@@ -32,8 +44,17 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Form submitted', this.loginForm.value);
-      // Add your login logic here
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          console.log('Login successful');
+          this.router.navigate(['/']); // Navigate to homepage after successful login
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+          this.errorMessage = 'Login failed. Please check your credentials and try again.'
+        },
+      });
     }
   }
 }
