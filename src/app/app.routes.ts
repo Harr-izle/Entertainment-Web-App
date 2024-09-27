0fb7156693403fc07b8ccffe708c4c653b0698c2
Harr-izle <harrietbuadee@gmail.com>
2024-09-27 15:41:08 +0000

    updates routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,23 @@
-import { SignupComponent } from './components/signup/signup.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: SignupComponent
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { 
+    path: '', 
+    component: HomeComponent, 
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: 'all', pathMatch: 'full' },
+      { path: 'all', component: HomeComponent },
+      { path: 'movie', component: HomeComponent },
+      { path: 'tv', component: HomeComponent },
+      { path: 'bookmarks', component: HomeComponent }
+    ]
   },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'movie',
-    component: HomeComponent
-  },
-  {
-    path: 'tv',
-    component: HomeComponent
-  },
-  {
-    path: 'bookmarks',
-    component: HomeComponent
-  }
+  { path: '**', redirectTo: '' }
 ];
